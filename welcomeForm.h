e160e61a257cb096a036ab599db8e7556606f8ee
Farah Moataz <farahmoataz90@gmail.com>
2023-05-05 16:58:23 +0300

    gui for the welcome page

--- welcomeForm.h
+++ welcomeForm.h
@@ -37,6 +37,19 @@ namespace UMS {
 
 	private: System::Windows::Forms::Button^ studentbt;
 	private: System::Windows::Forms::Button^ adminbt;
+
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::Label^ label9;
 	protected:
 
 	protected:
@@ -58,18 +71,40 @@ namespace UMS {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(welcomeForm::typeid));
 			this->studentbt = (gcnew System::Windows::Forms::Button());
 			this->adminbt = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->panel2->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// studentbt
 			// 
-			this->studentbt->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
-			this->studentbt->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->studentbt->BackColor = System::Drawing::Color::DodgerBlue;
+			this->studentbt->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->studentbt->FlatAppearance->BorderSize = 0;
+			this->studentbt->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->studentbt->Font = (gcnew System::Drawing::Font(L"Segoe UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->studentbt->Location = System::Drawing::Point(388, 135);
+			this->studentbt->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(224)), static_cast<System::Int32>(static_cast<System::Byte>(224)),
+				static_cast<System::Int32>(static_cast<System::Byte>(224)));
+			this->studentbt->Location = System::Drawing::Point(508, 342);
+			this->studentbt->Margin = System::Windows::Forms::Padding(2);
 			this->studentbt->Name = L"studentbt";
-			this->studentbt->Size = System::Drawing::Size(282, 256);
+			this->studentbt->Size = System::Drawing::Size(119, 32);
 			this->studentbt->TabIndex = 1;
 			this->studentbt->Text = L"Student";
 			this->studentbt->UseVisualStyleBackColor = false;
@@ -77,27 +112,196 @@ namespace UMS {
 			// 
 			// adminbt
 			// 
-			this->adminbt->BackColor = System::Drawing::SystemColors::GradientActiveCaption;
-			this->adminbt->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->adminbt->BackColor = System::Drawing::Color::DodgerBlue;
+			this->adminbt->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->adminbt->FlatAppearance->BorderSize = 0;
+			this->adminbt->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->adminbt->Font = (gcnew System::Drawing::Font(L"Segoe UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->adminbt->Location = System::Drawing::Point(34, 135);
+			this->adminbt->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(224)), static_cast<System::Int32>(static_cast<System::Byte>(224)),
+				static_cast<System::Int32>(static_cast<System::Byte>(224)));
+			this->adminbt->Location = System::Drawing::Point(642, 342);
+			this->adminbt->Margin = System::Windows::Forms::Padding(2);
 			this->adminbt->Name = L"adminbt";
-			this->adminbt->Size = System::Drawing::Size(282, 256);
+			this->adminbt->Size = System::Drawing::Size(107, 32);
 			this->adminbt->TabIndex = 2;
 			this->adminbt->Text = L"Admin";
 			this->adminbt->UseVisualStyleBackColor = false;
 			this->adminbt->Click += gcnew System::EventHandler(this, &welcomeForm::adminbt_Click);
 			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(931, 0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(39, 29);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &welcomeForm::pictureBox1_Click);
+			// 
+			// panel2
+			// 
+			this->panel2->Controls->Add(this->pictureBox2);
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Left;
+			this->panel2->Location = System::Drawing::Point(0, 0);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(468, 489);
+			this->panel2->TabIndex = 4;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(0, 0);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(468, 489);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox2->TabIndex = 0;
+			this->pictureBox2->TabStop = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 20.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::SteelBlue;
+			this->label1->Location = System::Drawing::Point(474, 98);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(322, 37);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Discover Your Potential ";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 20.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::SystemColors::ActiveCaption;
+			this->label2->Location = System::Drawing::Point(474, 135);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(180, 37);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"at University";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::Gray;
+			this->label3->Location = System::Drawing::Point(478, 187);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(392, 13);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"At University, we believe in the potential of every student. Our inclusive and ";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->ForeColor = System::Drawing::Color::Gray;
+			this->label4->Location = System::Drawing::Point(478, 200);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(465, 13);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"supportive environment encourages you to explore your interests, challenge yourse"
+				L"lf, and ";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->ForeColor = System::Drawing::Color::Gray;
+			this->label5->Location = System::Drawing::Point(478, 213);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(438, 13);
+			this->label5->TabIndex = 9;
+			this->label5->Text = L"grow academically and personally. With world-renowned faculty and state-of-the-ar"
+				L"t ";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->ForeColor = System::Drawing::Color::Gray;
+			this->label6->Location = System::Drawing::Point(478, 226);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(434, 13);
+			this->label6->TabIndex = 10;
+			this->label6->Text = L"facilities, we provide the tools you need to succeed in any field. Our vibrant ca"
+				L"mpus ";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label7->ForeColor = System::Drawing::Color::Gray;
+			this->label7->Location = System::Drawing::Point(478, 239);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(434, 13);
+			this->label7->TabIndex = 11;
+			this->label7->Text = L"community offers countless opportunities for extracurricular involvement. Join us"
+				L" at ";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label8->ForeColor = System::Drawing::Color::Gray;
+			this->label8->Location = System::Drawing::Point(478, 252);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(201, 13);
+			this->label8->TabIndex = 12;
+			this->label8->Text = L"University and discover your potential";
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Font = (gcnew System::Drawing::Font(L"Segoe UI", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label9->ForeColor = System::Drawing::Color::Gray;
+			this->label9->Location = System::Drawing::Point(482, 306);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(145, 25);
+			this->label9->TabIndex = 13;
+			this->label9->Text = L"Who you are \?!";
+			// 
 			// welcomeForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(706, 540);
+			this->BackColor = System::Drawing::Color::White;
+			this->ClientSize = System::Drawing::Size(969, 489);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->adminbt);
 			this->Controls->Add(this->studentbt);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"welcomeForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"welcomeForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->panel2->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -120,5 +324,8 @@ namespace UMS {
 		//close welcome Form
 		this->Close();
 	}
-	};
+	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Close();
+	}
+};
 }
